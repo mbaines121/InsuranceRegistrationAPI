8e85bf41ef35b3b3c06d079ca7368f40f2f9aadb
Mark Baines <markbaines121@hotmail.com>
2025-02-27 12:13:11 +0000

    Added support for SQL Server aspire host.

--- src/InsuranceRegistration.AppHost/Program.cs
+++ src/InsuranceRegistration.AppHost/Program.cs
@@ -1,5 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.InsuranceRegistration_API>("insuranceregistration-api");
+var sql = builder.AddSqlServer("sql")
+                 .WithLifetime(ContainerLifetime.Persistent)
+                 .WithEndpoint(1433, 1433, name: "ssms")
+                 .WithDataVolume();
+
+var db = sql.AddDatabase("InsuranceRegistrationDb");
+
+builder.AddProject<Projects.InsuranceRegistration_API>("insuranceregistration-api")
+    .WithReference(db)
+    .WaitFor(db);
 
 builder.Build().Run();
